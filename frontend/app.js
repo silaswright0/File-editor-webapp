d01d933608c685b1a41164ec7e01d9c56d3b7cc0
silaswright0 <sir_silas123@outlook.com>
2026-06-22 23:25:57 -0400

    started on upload logic

--- frontend/app.js
+++ frontend/app.js
@@ -6,3 +6,45 @@ document.addEventListener('DOMContentLoaded'), () => {
     const editor = new PdfEditor('editor-tools');
     editor.initTools();
 }
+
+document.getElementById('upload-btn').addEventListener('click', async () => {
+    const fileInput = document.getElementById('pdf-file-input');
+    const statusMessage = document.getElementById('status-message');
+
+    if  (fileInput.isDefaultNamespace.length === 0){
+        statusMessage.innerText = "Please upload a PDF file first";
+        statusMessage.style.color = "red";
+        return;
+    }
+
+    const file = fileInput.files[0];//take only one file
+
+    const formData = new FormData();
+    formData.append('pdf_file', file);
+
+    statusMessage.innerText = "Uploading...";
+    statusMessage.style.color = "blue";
+
+    try {
+        const response = await fetch('http://localhost:5000/api/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            statusMessage.innerText = 'Success! Saved as: ${data.filename}';
+            statusMessage.style.color = "green";
+
+            //pass file to pdf.js to render on screen
+        } else {
+            statusMessage.innerText = 'Upload failed: ${data.error}';
+            statusMessage.style.color = "red";
+        }
+    }catch (error){
+        console.error("error uploading file:", error);
+        statusMessage.innerText = "server error, is your python backend configured and running?";
+        statusMessage.style.color = "red";
+    }
+});
--- server.py
+++ server.py
@@ -2,6 +2,14 @@ from flask import Flask, send_from_directory, jsonify, request
 import os
 
 app = Flask(__name__, static_folder='frontend')
+STORAGE_DIR = "storage"
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    return response
 
 @app.route("/")
 def home():
@@ -15,10 +23,21 @@ def serve_static(path):
 
 @app.route('/api/pdf/upload', methods=['POST'])
 def upload_pdf():
+
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'ok'}), 200
+
     if 'file' not in request.files:
         return jsonify({'error': 'No file provided'}), 400
 
     file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'empty filename'}), 400
+    safe_filename = os.path.basename(file.filename).replace("..","")
+
+    file_binary_data = file.read()
+    with open(destination_path, "wb") as f:
+        f.write(file_binary_data)
 
     return jsonify({
         'message': 'File uploaded successfully',
